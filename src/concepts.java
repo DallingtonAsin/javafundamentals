8592ba9cc98d414f9c29555eff3f7b69ae987f48
Dallington <asingwire50dallington@gmail.com>
2021-05-04 13:17:18 +0300

    updated concepts file

--- src/concepts.java
+++ src/concepts.java
@@ -1,34 +1,27 @@
-/*
 
-List of Java Fundamentals implemented here
-
-1. Inheritance
-2. Abstraction
-3. Encapsulation
-4. Polymorphism
-5. Exception Handling
-
-
-*/
-import java.util.*;
 public class concepts {
 	
-	public String name;
-	public int age;
+	/*********
 
-	public static void main(String[] args) {
-		
-		concepts obj = new concepts(); // Object creation / class instantiation
-		Scanner input = new Scanner(System.in); // Read input from keyboard
-		
-		System.out.println("Enter your name");
-		obj.name = input.nextLine();
-		System.out.println("Enter your age");
-		obj.age = input.nextInt();
-		
-		System.out.print("The entered name is "+obj.name+" and age is "+obj.age);
-		input.close();
-		
-	}
+	List of Java Fundamentals implemented within this repository
+	Please check in the respective folders/packages under src folder
+	to trace the concept.
+
+	1. Inheritance
+	2. Abstraction
+	3. Encapsulation
+	4. Polymorphism
+	5. Exception Handling
+	6. Data Structures (Arrays, Linked lists, Stack, Queues and Recursion)
+	
+	
+	********************************************************
+	More concepts will always be added, please always check and clone
+	this repository at anytime. I wish you happy learning!
+	************************************************************
+
+
+	*********/
+
 
 }
