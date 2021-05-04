cf9773b3f1f6f1b33f349e003a6e38e10b78e9c9
Dallington <asingwire50dallington@gmail.com>
2021-05-04 13:36:12 +0300

    added exception handling

---
+++ src/ExceptionHandling/Example.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+
+public class Example {
+	
+	public void divide(double a, double b) {
+		double result;
+		try {
+			result =  a / b; 
+			System.out.println("The result of "+a+" / "+b+" is "+result);
+			
+		}catch(Exception ex) {
+			 System.out.println("Caught exception "+ex.getMessage());
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Example example = new Example();
+		
+		example.divide(70, 0);
+
+	}
+
+}
---
+++ src/ExceptionHandling/ReadMeFirst.java
@@ -0,0 +1,21 @@
+package ExceptionHandling;
+
+public class ReadMeFirst {
+	
+	/******
+	 
+	Exception handling is a mechanism to handle runtime errors such as
+	ClassNotFoundException, IOException, SQLException, RemoteEception so that normal flow 
+	of the application can be maintained
+	
+	****Hierarchy of java exceptions********
+	
+	Throwable->Eception-> (IOException, SQLException, ClassNotFoundException and RuntimeException)
+	From RuntimeException->(ArithmeticException, NullPointerException, NumberFormatException and 
+	IndexOutOfBoundsException->(ArrayIndexOutOfBoundsException and StringIndexOutOfBoundsException))
+	
+	*Error->(StackOverflowError, VirtualMachineError, OutOfMemoryError)
+	 
+	 *****/
+
+}
